d84348c8d30a27df894439ce954a1fdc7c4d1f78
aman_sama <amanrai2002acr@gmail.com>
2025-07-21 23:22:48 +0545

    clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignInButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      HomePage
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </div>
+  );
 };
 
 export default Home;
